e34d9ed6192bf3667529badf2736db213ab79942
i509VCB <git@i509.me>
2022-03-26 23:14:42 -0500

    remove useless dependencies

--- compositor/build.rs
+++ compositor/build.rs
@@ -1,4 +1,4 @@
-use std::{env, fs::OpenOptions, io::Write};
+use std::env;
 
 fn main() {
     if env::var("CARGO_FEATURE_LOGIND").ok().is_none() && env::var("CARGO_FEATURE_LIBSEAT").ok().is_none() {
@@ -10,31 +10,5 @@ fn main() {
         println!("cargo:warning=$ cargo run --feature logind");
         println!("cargo:warning=To enable libseat support add `--feature libseat` to your cargo invocation.");
         println!("cargo:warning=$ cargo run --feature libseat");
-    }
-
-    // Shader compilation
-    {
-        let shader_path = env::current_dir()
-            .unwrap()
-            .join("src")
-            .join("vulkan")
-            .join("renderer")
-            .join("shader");
-
-        let mut compiler = shaderc::Compiler::new().unwrap();
-
-        // Vertex shader
-        let vertex_shader = include_str!("src/vulkan/renderer/shader/vert.glsl");
-        let compiled_vertex = compiler
-            .compile_into_spirv(&vertex_shader, shaderc::ShaderKind::Vertex, "vert.glsl", "main", None)
-            .unwrap();
-        {
-            let mut file = OpenOptions::new()
-                .write(true)
-                .truncate(false)
-                .open(shader_path.join("vert.spv"))
-                .unwrap();
-            file.write_all(compiled_vertex.as_binary_u8()).unwrap();
-        }
     }
 }
